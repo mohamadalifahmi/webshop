3791e6b787e3da58ac3b1df3fdcecef157421de9
Mohamad Ali Fahmi <mf627137mohamad@gmail.com>
2026-08-30 15:52:25 +0300

    fix: completely remove horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use App\Providers\HorizonServiceProvider;
 use App\Providers\VoltServiceProvider;
 
 return [
     AppServiceProvider::class,
-    HorizonServiceProvider::class,
     VoltServiceProvider::class,
 ];
